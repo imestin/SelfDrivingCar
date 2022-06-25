1cb93c6267f295d0e2cd33f5d883f20116c636c0
Narayana Imestin <imestin@tuta.io>
2022-06-25 21:07:34 +0200

    Adds mutation

--- main.js
+++ main.js
@@ -9,11 +9,35 @@ const road = new Road(carCanvas.width/2, carCanvas.width*0.9);
 //const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
 const N = 100;
 const cars = generateCars(N);
+let bestCar = cars[0];
+if (localStorage.getItem("bestBrain")) {
+  //bestCar.brain = JSON.parse(localStorage.getItem("bestBrain"))
+  for(let i = 0; i < cars.length; i++) {
+    cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"));
+    if (i != 0) {
+      NeuralNetwork.mutate(cars[i].brain, 0.1);
+    }
+  }
+}
 const traffic = [
   new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2),
 ];
 
 animate();
+
+function save() {
+  localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
+}
+
+function discard() {
+  localStorage.removeItem("bestBrain")
+}
 
 function generateCars(N) {
   const cars = [];
@@ -32,7 +56,7 @@ function animate(time) {
     cars[i].update(road.borders, traffic);
   }
 
-  const bestCar = cars.find(
+  bestCar = cars.find(
     c => c.y == Math.min(...cars.map(c => c.y))
   );
   
